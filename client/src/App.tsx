20a10d7d5845404a359522ea1d80dbd3823fda51
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-01-17 21:43:50 +0600

    feat(product): add create product UI and axios auth token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import useAuthReq from "./hooks/useAuthReq";
 
 function App() {
+  const { isSignedIn } = useAuthReq();
   return (
     <div className="min-h-screen bg-base-100">
       <Navbar />
       <main className="max-w-6xl mx-auto px-4 py-6">
         <Routes>
           <Route path="/" element={<HomePage />} />
+
+          <Route
+            path="/create"
+            element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />}
+          />
         </Routes>
       </main>
     </div>
